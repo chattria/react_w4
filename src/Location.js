15f61b7579fc1f7c0c37e811f0ec8189f04e53ac
Chattria T. Palm <86838286+chattria@users.noreply.github.com>
2023-04-21 13:26:46 +0200

    Location
    Fixed bootstrap to the code and added Location component

---
+++ src/Location.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function Location() {
+  return (
+    <Card>
+      <Card.Title>Location</Card.Title>
+      <Card.Body>
+        <p>Sweden</p>
+        <p>Stockholm</p>
+      </Card.Body>
+    </Card>
+  );
+}
--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,5 +1,21 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Location from "./Location";
 
 export default function WeatherApp() {
-  return <h1>Welcome</h1>;
+  return (
+    <Container>
+      <Row xs={3}>
+        <Col sm>
+          <Location />
+        </Col>
+        <Col sm>col 2</Col>
+        <Col sm>col 3</Col>
+      </Row>
+    </Container>
+  );
 }
