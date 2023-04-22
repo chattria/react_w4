11d678741fefab6798601b23f3667e696a4e3087
Chattria T. Palm <86838286+chattria@users.noreply.github.com>
2023-04-23 00:36:12 +0200

    Theme Button
    Coded so that the theme button works

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,5 +1,6 @@
 import "./WeatherApp.css";
 import React from "react";
+import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -70,6 +71,18 @@ export default function WeatherApp() {
               </span>
             </p>
           </Card>
+
+          <Form className="search-city">
+            <button id="search-btn" type="submit">
+              search
+            </button>
+            <input
+              type="search"
+              placeholder=" city. . ."
+              id="city-input"
+              autocomplete="off"
+            />
+          </Form>
         </Col>
         <Col sm className="main-col">
           <Card>
