ed581bb2d78b838ce1865cbe321103c62afea585
Chattria T. Palm <86838286+chattria@users.noreply.github.com>
2023-04-22 23:24:29 +0200

    Moved components to one componet
    It started to get to many, so I decided to move the code to one component

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,22 +1,138 @@
+import "./WeatherApp.css";
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-import Location from "./Location";
-import Info from "./Info";
 
 export default function WeatherApp() {
   return (
-    <Container>
+    <Container fluid>
       <Row xs={3}>
-        <Col sm>
-          <Location />
-          <Info />
+        <Col sm className="main-col">
+          <Card>
+            <Card.Header>Location</Card.Header>
+            <Card.Body>
+              <p>Sweden</p>
+              <p>Stockholm</p>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Header>Info</Card.Header>
+            <Card.Body>
+              <Container>
+                <Row>
+                  <Col>
+                    <ul>
+                      <li>Low</li>
+                      <li>High</li>
+                      <li>Wind</li>
+                      <li>Humidity</li>
+                    </ul>
+                  </Col>
+                  <Col>
+                    <ul>
+                      <li>
+                        <span id="low"></span>
+                        <span class="unit"> ºc</span>
+                      </li>
+                      <li>
+                        <span id="high"></span>
+                        <span class="unit"> ºc</span>
+                      </li>
+                      <li>
+                        <span id="wind"></span>km/h
+                      </li>
+                      <li>
+                        <span id="humidity"></span>%
+                      </li>
+                    </ul>
+                  </Col>
+                </Row>
+              </Container>
+            </Card.Body>
+          </Card>
         </Col>
-        <Col sm>col 2</Col>
-        <Col sm>col 3</Col>
+        <Col sm className="main-col">
+          <Card className="temp-card">
+            <p class="description"></p>
+            <p id="temp-num">
+              <span id="temp"></span>
+              <span id="temp-unit" class="unit">
+                {" "}
+                ºc
+              </span>
+            </p>
+          </Card>
+        </Col>
+        <Col sm className="main-col">
+          <Card>
+            <Card.Header className="date">2023-04-22</Card.Header>
+            <Card.Body>
+              <p id="today">Friday</p>
+              <p id="time">22:41</p>
+            </Card.Body>
+          </Card>
+
+          <Card>
+            <Card.Header id="card-forcast">Forecast</Card.Header>
+            <Card.Body className="text-center">
+              <Container>
+                <Row>
+                  <Col>
+                    <ul>
+                      <li className="li-week" id="tomorrow"></li>
+                      <li className="li-week" id="day-after-tomorrow"></li>
+                      <li className="li-week" id="third-day"></li>
+                      <li className="li-week" id="fourth-day"></li>
+                      <li className="li-week" id="fifth-day"></li>
+                      <li className="li-week" id="sixth-day"></li>
+                    </ul>
+                  </Col>
+                  <Col>
+                    <li>
+                      <span className="forecast" id="tom-degree">
+                        0
+                      </span>
+                      <span className="unit"> ºc</span>
+                    </li>
+                    <li>
+                      <span className="forecast" id="day-degree">
+                        0
+                      </span>
+                      <span className="unit"> ºc</span>
+                    </li>
+                    <li>
+                      <span className="forecast" id="thi-degree">
+                        0
+                      </span>
+                      <span className="unit"> ºc</span>
+                    </li>
+                    <li>
+                      <span className="forecast" id="fou-degree">
+                        0
+                      </span>
+                      <span className="unit"> ºc</span>
+                    </li>
+                    <li>
+                      <span className="forecast" id="fif-degree">
+                        0
+                      </span>
+                      <span className="unit"> ºc</span>
+                    </li>
+                    <li>
+                      <span className="forecast" id="six-degree">
+                        0
+                      </span>
+                      <span className="unit"> ºc</span>
+                    </li>
+                  </Col>
+                </Row>
+              </Container>
+            </Card.Body>
+          </Card>
+        </Col>
       </Row>
     </Container>
   );
