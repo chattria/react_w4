5a757acd452afe7cfb52a72cfb3afd3a91bf890c
Chattria T. Palm <86838286+chattria@users.noreply.github.com>
2023-04-24 11:08:48 +0200

    Footer
    Added footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import React from "react";
 import WeatherApp from "./WeatherApp";
 import ThemeButton from "./ThemeButton";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
       <ThemeButton />
       <WeatherApp />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <p id="info">
+      Project coded by Chattria T. Palm and is{" "}
+      <a
+        href="https://github.com/chattria/react_w4"
+        target="_blank"
+        id="info-link"
+      >
+        open-sourced on GitHub
+      </a>
+    </p>
+  );
+}
