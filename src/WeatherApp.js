c2093d0a4a20e7073196b6ef98dbb4cb259b801c
Chattria T. Palm <86838286+chattria@users.noreply.github.com>
2023-04-26 09:16:49 +0200

    Bootstrap
    Fixed bootstrap and the css so that it works with the bootstrap

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,72 +1,25 @@
 import "./WeatherApp.css";
-import React, { useState } from "react";
-import axios from "axios";
-import InputGroup from "react-bootstrap/InputGroup";
+import React from "react";
+
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import InputGroup from "react-bootstrap/InputGroup";
 
-export default function Weather() {
-  let [city, setCity] = useState("");
-  let [weather, setWeather] = useState({});
-  let [loading, setLoading] = useState(false);
-
-  function showWeather(response) {
-    console.log(response.data);
-    setLoading(true);
-    setWeather({
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
-      country: response.data.sys.country,
-      city: response.data.name,
-      temperature: Math.round(response.data.main.temp),
-      low: Math.floor(response.data.main.temp_min),
-      high: Math.round(response.data.main.temp_max),
-      wind: Math.round(response.data.wind.speed),
-      humidity: response.data.main.humidity,
-      description: response.data.weather[0].description,
-    });
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    let apiKey = "0a521eaf234a3a56f45252fac3c737";
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}ad&units=metric`;
-    axios.get(url).then(showWeather);
-  }
-
-  function updateCity(event) {
-    setCity(event.target.value);
-  }
-
-  let form = (
-    <Form onSubmit={handleSubmit} className="search-city">
-      <InputGroup>
-        <Button type="submit" className="search-btn">
-          search
-        </Button>
-        <Form.Control
-          type="search"
-          placeholder="  Type a city"
-          id="city-input"
-          onChange={updateCity}
-        />
-      </InputGroup>
-    </Form>
-  );
-
-  if (loading) {
-    return (
+export default function WeatherApp() {
+  return (
+    <div className="WeatherApp">
       <Container fluid className="Weather">
-        <Row xs={3}>
+        <Row>
           <Col sm className="main-col">
             <Card>
               <Card.Header>Location</Card.Header>
               <Card.Body>
-                <p id="country">{weather.country}</p>
-                <p id="city">{weather.city}</p>
+                <p id="country"></p>
+                <p id="city"></p>
               </Card.Body>
             </Card>
             <Card>
@@ -85,18 +38,18 @@ export default function Weather() {
                     <Col>
                       <ul>
                         <li>
-                          <span id="low">{weather.low}</span>
-                          <span class="unit"> ºc</span>
+                          <span id="low"></span>
+                          <span className="unit"> ºc</span>
                         </li>
                         <li>
-                          <span id="high">{weather.high}</span>
-                          <span class="unit"> ºc</span>
+                          <span id="high"></span>
+                          <span className="unit"> ºc</span>
                         </li>
                         <li>
-                          <span id="wind">{weather.wind}</span>km/h
+                          <span id="wind"></span>km/h
                         </li>
                         <li>
-                          <span id="humidity">{weather.humidity}</span>%
+                          <span id="humidity"></span>%
                         </li>
                       </ul>
                     </Col>
@@ -104,32 +57,44 @@ export default function Weather() {
                 </Container>
               </Card.Body>
             </Card>
-            <div class="unit-button">
-              <button class="change-button" id="current-btn">
+            <div className="unit-button">
+              <Button className="change-button" id="current-btn">
                 current
-              </button>
+              </Button>
             </div>
           </Col>
           <Col sm className="main-col">
             <Card className="temp-card">
-              <img src={weather.icon} alt={weather.description} />
-              <p class="description">{weather.description}</p>
+              <p className="description"></p>
               <p id="temp-num">
-                <span id="temp">{weather.temperature}</span>
-                <span id="temp-unit" class="unit">
+                <span id="temp"></span>
+                <span id="temp-unit" className="unit">
                   {" "}
                   ºc
                 </span>
               </p>
             </Card>
-            {form}
+
+            <Form className="search-city">
+              <InputGroup>
+                <Button type="submit" className="search-btn">
+                  search
+                </Button>
+                <Form.Control
+                  type="search"
+                  placeholder="  Type a city"
+                  id="city-input"
+                  autoFocus="on"
+                />
+              </InputGroup>
+            </Form>
           </Col>
           <Col sm className="main-col">
             <Card>
-              <Card.Header className="date">{weather.today}</Card.Header>
+              <Card.Header className="date"></Card.Header>
               <Card.Body>
-                <p id="today">{weather.date}</p>
-                <p id="time">{weather.time}</p>
+                <p id="today"></p>
+                <p id="time"></p>
               </Card.Body>
             </Card>
 
@@ -181,8 +146,6 @@ export default function Weather() {
           </Col>
         </Row>
       </Container>
-    );
-  } else {
-    return form;
-  }
+    </div>
+  );
 }
